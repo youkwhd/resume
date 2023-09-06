c0ca591bbcbc33b832c4a669552f210e9274d5db
youkwhd <lolywk@tutanota.com>
2023-09-07 03:02:54 +0700

    chore: private key reading

---
+++ generate.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use Crypt::Mode::ECB;
+
+sub get_private_key {
+    my $private_key_file = "private-key";
+    my $fh;
+
+    die "FATAL: Cannot find $private_key_file\n" if not open $fh, "<", $private_key_file;
+
+    my $key = trim(<$fh>);
+
+    die "FATAL: Error during file closing\n" if not close $fh;
+
+    $key;
+}
+
+sub trim {
+    $_[0] =~ s/^\s+|\s+$//g;
+    $_[0];
+}
+
+sub bin_to_hex_little ($) {
+    unpack "H*", $_[0];
+}
+
+my $key = get_private_key();
+my $crypt = Crypt::Mode::ECB->new("AES");
+
+my $cp = $crypt->encrypt("Hello from Perl", $key);
+my $dc = $crypt->decrypt($cp, $key);
+
+print bin_to_hex_little($cp), "\n", "$dc\n";
