8c13d42b7d6fc26c8bd1c56e401dbed91e9289aa
youkwhd <lolywk@tutanota.com>
2023-09-07 16:15:17 +0700

    chore: generate using templating

--- gen-resume.pl
+++ gen-resume.pl
@@ -1,29 +1,16 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -I.
 
 require "common.pl";
+require "key.pl";
 
 use warnings;
 use strict;
-use Crypt::Mode::ECB;
+use Data::Dumper;
+use YAML::XS "LoadFile";
+use Template;
+use File::Slurp;
 
-sub get_private_key {
-    my $private_key_file = "private-key";
-    my $fh;
+my $tt = Template->new();
+my $data = LoadFile("data.yaml");
 
-    die "FATAL: Cannot find $private_key_file\n" if not open $fh, "<", $private_key_file;
-
-    my $key = <$fh>;
-    Common::trim $key;
-
-    die "FATAL: Error during file closing\n" if not close $fh;
-
-    $key;
-}
-
-my $key = get_private_key();
-my $crypt = Crypt::Mode::ECB->new("AES");
-
-my $cp = $crypt->encrypt("Hello from Perl", $key);
-my $dc = $crypt->decrypt($cp, $key);
-
-print Common::bin_to_hex_little($cp), "\n", "$dc\n";
+$tt->process("resume.template.tex", $data, my $resume);
