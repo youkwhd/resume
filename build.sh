2236876e198914edf22d62c9362ebf71b53eb2e7
youkwhd <lolywk@tutanota.com>
2023-09-07 16:37:01 +0700

    feat: automatically check for needed files

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/sh
 
+file_check_or_exit() {
+    local filename="$1"
+
+    if [ ! -e $filename ]; then
+        >&2 echo "ERROR: No $filename found."
+        exit 1
+    fi
+}
+
+file_check_or_exit private-key
+
+if [ ! -e data.public.yaml ]; then
+    file_check_or_exit data.yaml
+    ./gen-data.pl --encrypt
+fi
+
 ./gen-resume.pl | pdflatex --jobname=resume
