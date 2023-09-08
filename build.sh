c183ca21553fb9d6aa1cc0784f66a41a3cee42bb
youkwhd <lolywk@tutanota.com>
2023-09-08 15:10:43 +0700

    chore: private-key is optional

--- build.sh
+++ build.sh
@@ -1,24 +1,3 @@
 #!/usr/bin/sh
 
-file_check_or_exit() {
-    local filename="$1"
-
-    if [ ! -e $filename ]; then
-        >&2 echo "ERROR: No $filename found."
-        exit 1
-    fi
-}
-
-file_check_or_exit private-key
-
-if [ ! -e data.public.yaml ]; then
-    file_check_or_exit data.yaml
-    ./bin/gen-data.pl --encrypt
-fi
-
-if [ ! -e data.yaml ]; then
-    file_check_or_exit data.public.yaml
-    ./bin/gen-data.pl --decrypt
-fi
-
 ./bin/gen-resume.pl | pdflatex --jobname=resume
