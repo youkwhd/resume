d221e83f43a29d75862d47ab9ba7e11d143181a1
youkwhd <lolywk@tutanota.com>
2023-09-10 14:17:56 +0700

    feat: check if data.yaml file exists

--- bin/gen-resume.pl
+++ bin/gen-resume.pl
@@ -18,6 +18,18 @@ sub generate_and_print_resume ($) {
     $tt->process(Constants->RESUME_TEMPLATE_FILENAME, $data, my $resume);
 }
 
+sub usage {
+    print STDERR
+        "Usage: $0\n",
+        "Generates TeX file from resume.template.tex with data/data.yaml\n";
+}
+
+unless (-e Constants->DATA_FILENAME) {
+    usage;
+    print STDERR "ERROR: no " . Constants->DATA_FILENAME . " found, see: README\n";
+    exit 1;
+}
+
 generate_and_print_resume
     (($nargs >= 1 && $ARGV[0] eq "--example") ?
         Constants->DATA_EXAMPLE_FILENAME :
